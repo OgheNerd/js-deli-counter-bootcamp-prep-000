711658c8a6160ba67a2f0a52fd169bd74b558cb8
Oghene Michael <zonalmeet@gmail.com>
2018-02-09 15:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function currentLine(line, person) {
   if (!line.length) {
     return `The line is currently empty.`
   }
-  
+
+  const numberName =  []
+
   for (let i = 0; l = line.length; i < l; i++) {
     // return `The line is currently: ${}`
   }
